ec65fba793e00e15aa8db6b690c998befda24976
Francisco Oliveira <francisco55galo@icloud.com>
2023-05-22 16:20:51 -0300

    Criacao da Classe C

---
+++ ClasseC.ts
@@ -0,0 +1,17 @@
+export default class ClasseC {
+    private C1: string;
+    private C2: number;
+
+    public constructor () {
+        this.C1 = "";
+        this.C2 = 0;
+    }
+
+    public MC1 (): void {
+        console.log("Metodo MC1");
+    }
+
+    public MC2 (): void {
+        console.log("Metodo MC2");
+    }
+}
