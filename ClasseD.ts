61f2e6c1b961eee113bb245ea570b88a47eeee0e
Francisco Oliveira <francisco55galo@icloud.com>
2023-05-22 16:51:43 -0300

    Criando Classe D

---
+++ ClasseD.ts
@@ -0,0 +1,17 @@
+export default class ClasseD {
+    private D1: number;
+    private D2: number;
+
+    public constructor () {
+        this.D1 = 0;
+        this.D2 = 0;
+    }
+
+    public MD1 (): number {
+        return this.D1 + this.D2;
+    }
+
+    public MD2 (): boolean {
+        return this.D1 === this.D2;
+    }
+}
